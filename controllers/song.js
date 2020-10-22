66427791f55d6ea62139e0f8f9f892e261da68ce
Hannah Catherine Trask <hctrask@gmail.com>
2020-10-22 14:30:54 -0600

    seed data made

--- controllers/song.js
+++ controllers/song.js
@@ -1,6 +1,21 @@
 const Song = require("../models/song")
 const { Router } = require("express")
 const router = Router()
+const songData = require('../db/seedData.json');
+
+//SEED ROUTE
+router.get('/seed', async (req, res) => {
+	try {
+		await Song.deleteMany({});
+		const songs = await Song.insertMany(songData);
+		res.json({ status: 200, data: songs });
+	} catch (err) {
+		res.status(400).json({ err });
+	}
+});
+
+
+
 
 //index route
 router.get("/", async (req, res) => {
