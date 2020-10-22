072f51e61c6b32e8b4692faa44aac75d1c9a89a0
Chase Wood <chase@Chases-MacBook-Pro.local>
2020-10-22 13:54:43 -0500

    feature - created connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,16 @@
-test;
+// ENVIRONMENTAL VARIABLES //
+require('dotenv').config();
+
+// MONGOOSE CONNECTION //
+const { MONGODBURI } = process.env;
+const mongoose = require('mongoose');
+const config = { useUnifiedTopology: true, useNewUrlParser: true };
+const db = mongoose.connection;
+
+mongoose.connect(MONGODBURI, config);
+
+db.on('open', () => console.log('You are connected to Mongo'))
+	.on('close', () => console.log('You are connected to Mongo'))
+	.on('error', () => console.log('You are connected to Mongo'));
+
+module.exports = mongoose;
